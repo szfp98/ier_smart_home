82b264eae8555954d9ef7b08c1d29850ab6fc739
szfp98 <pal.szabofeltothy@gmail.com>
2024-05-04 20:41:00 +0200

    Create environment

---
+++ src/env/controller/SmartHomeEnvironment.java
@@ -0,0 +1,71 @@
+package controller;
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import model.SmartHomeModel;
+
+import java.util.logging.Logger;
+
+public class SmartHomeEnvironment extends Environment {
+    private SmartHomeModel model;
+    private final Logger logger = Logger.getLogger(SmartHomeEnvironment.class.getName());
+
+    public static final Literal TOGGLE_LIGHT = Literal.parseLiteral("toggleLight(light)");
+    public static final Literal SET_TEMPERATURE = Literal.parseLiteral("setTemperature(temperature)");
+    public static final Literal OPEN_WINDOW = Literal.parseLiteral("openWindow(window)");
+    public static final Literal CLOSE_WINDOW = Literal.parseLiteral("closeWindow(window)");
+    public static final Literal OPEN_DOOR = Literal.parseLiteral("openDoor(door)");
+    public static final Literal CLOSE_DOOR = Literal.parseLiteral("closeDoor(door)");
+
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        model = new SmartHomeModel();
+        model.addObserver((o, arg) -> updatePercepts());
+        logger.info("SmartHomeEnvironment initialized with args.");
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+        String act = action.getFunctor();
+        try {
+            switch (act) {
+                case "toggleLight":
+                    model.toggleDeviceState("light");
+                    break;
+                case "setTemperature":
+                    model.setTemperature(Double.parseDouble(action.getTerm(0).toString()));
+                    break;
+                case "openWindow":
+                    model.setDeviceState("window", true);
+                    break;
+                case "closeWindow":
+                    model.setDeviceState("window", false);
+                    break;
+                case "openDoor":
+                    model.setDeviceState("door", true);
+                    break;
+                case "closeDoor":
+                    model.setDeviceState("door", false);
+                    break;
+                default:
+                    logger.severe("Action " + act + " not supported");
+                    return false;
+            }
+            logger.info("Executed action " + act + " by agent " + agName);
+            return true;
+        } catch (Exception e) {
+            logger.severe("Error executing action: " + e.getMessage());
+            return false;
+        }
+    }
+
+    private void updatePercepts() {
+        clearAllPercepts();
+        addPercept(Literal.parseLiteral("light(" + model.getDeviceState("light") + ")"));
+        addPercept(Literal.parseLiteral("temperature(" + model.getTemperature() + ")"));
+        addPercept(Literal.parseLiteral("window(" + model.getDeviceState("window") + ")"));
+        addPercept(Literal.parseLiteral("door(" + model.getDeviceState("door") + ")"));
+        logger.fine("Updated percepts based on the environment state.");
+    }
+}
