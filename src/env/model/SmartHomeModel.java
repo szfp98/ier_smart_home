5c8ae3b75a17d62985248e07fb7be5ecb80c11b2
szfp98 <pal.szabofeltothy@gmail.com>
2024-05-04 20:40:37 +0200

    Create model for MVC architecture

---
+++ src/env/model/SmartHomeModel.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
+import java.util.logging.Logger;
+
+public class SmartHomeModel extends Observable {
+    private Map<String, Boolean> deviceStates;
+    private double temperature;
+    private boolean isDaytime;
+    private final Logger logger = Logger.getLogger(SmartHomeModel.class.getName());
+
+    public SmartHomeModel() {
+        deviceStates = new HashMap<>();
+        initializeDeviceStates();
+    }
+
+    private void initializeDeviceStates() {
+        deviceStates.put("light", false);
+        deviceStates.put("airConditioner", false);
+        deviceStates.put("curtains", true);
+        deviceStates.put("door", false);
+        deviceStates.put("window", false);
+        temperature = 22.0;
+        isDaytime = true;
+        logger.info("Device states initialized.");
+    }
+
+    public boolean getDeviceState(String device) {
+        return deviceStates.getOrDefault(device, false);
+    }
+
+    public void setDeviceState(String device, boolean state) {
+        deviceStates.put(device, state);
+        setChanged();
+        notifyObservers(device);
+        logger.info("Device state updated: " + device + " to " + state);
+    }
+
+    public void toggleDeviceState(String device) {
+        boolean newState = !getDeviceState(device);
+        setDeviceState(device, newState);
+        logger.info("Toggled device state: " + device + " to " + newState);
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+        setChanged();
+        notifyObservers("temperature");
+        logger.info("Temperature set to " + temperature);
+    }
+
+    public boolean isDaytime() {
+        return isDaytime;
+    }
+
+    public void setDaytime(boolean isDaytime) {
+        this.isDaytime = isDaytime;
+        setChanged();
+        notifyObservers("daytime");
+        logger.info("Daytime set to " + isDaytime);
+    }
+}
